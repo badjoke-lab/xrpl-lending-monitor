16a8771246a6183c1fef94078ed585393a84d918
badjoke-lab <badjoke.lab@gmail.com>
2026-07-02 15:49:10 +0900

    feat: load Loan Broker page styles

--- src/ui/main.tsx
+++ src/ui/main.tsx
@@ -4,6 +4,7 @@ import { createRoot } from 'react-dom/client'
 import { App } from './App'
 import './styles.css'
 import './vaults.css'
+import './brokers.css'
 
 const rootElement = document.getElementById('root')
 
