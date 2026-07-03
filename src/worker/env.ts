988e3ca51b1bbf5d0c7566feb57837a27471199a
badjoke-lab <badjoke.lab@gmail.com>
2026-07-03 12:38:11 +0900

    Update binding type

--- src/worker/env.ts
+++ src/worker/env.ts
@@ -3,5 +3,5 @@ import type { RuntimeEnvironment } from '../shared/runtime-config'
 export interface Bindings extends RuntimeEnvironment {
   ASSETS: Fetcher
   DB: D1Database
-  CURRENT_STATE?: R2Bucket
+  CURRENT_STATE?: D1Database
 }
