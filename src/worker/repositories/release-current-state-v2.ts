6fe623d38dc7138095fed96d57bb8100119b4772
badjoke-lab <badjoke.lab@gmail.com>
2026-07-04 02:47:30 +0900

    Add release state compatibility exports

---
+++ src/worker/repositories/release-current-state-v2.ts
@@ -0,0 +1,2 @@
+export { openConfiguredReleaseCurrentState, resolveCurrentStateStorage, normalizeReleaseRecord } from './release-current-state'
+export type { CurrentStateStorage, ReleaseCurrentStateSource, ResolvedCurrentStateStorage } from './release-current-state'
