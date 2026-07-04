5ca4f35e402df87f796c8e3f8d252858e88721d3
badjoke-lab <badjoke.lab@gmail.com>
2026-07-05 05:13:19 +0900

    Expose scheduled collector runtime bindings

--- src/worker/env.ts
+++ src/worker/env.ts
@@ -1,6 +1,11 @@
+import type { CatchUpRuntimeEnvironment } from '../shared/catch-up-runtime-config'
+import type { IncrementalRuntimeEnvironment } from '../shared/incremental-runtime-config'
 import type { RuntimeEnvironment } from '../shared/runtime-config'
 
-export interface Bindings extends RuntimeEnvironment {
+export interface Bindings
+  extends RuntimeEnvironment,
+    IncrementalRuntimeEnvironment,
+    CatchUpRuntimeEnvironment {
   ASSETS: Fetcher
   DB: D1Database
 }
