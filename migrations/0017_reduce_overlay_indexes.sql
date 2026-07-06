1bd19fa1db69aecac75c2cc401843aea5fe398b9
badjoke-lab <badjoke.lab@gmail.com>
2026-07-06 12:28:07 +0900

    Reduce overlay write-amplifying indexes

---
+++ migrations/0017_reduce_overlay_indexes.sql
@@ -0,0 +1,10 @@
+PRAGMA foreign_keys = ON;
+
+DROP INDEX IF EXISTS current_state_overlay_objects_owner;
+DROP INDEX IF EXISTS current_state_overlay_objects_account;
+DROP INDEX IF EXISTS current_state_overlay_objects_borrower;
+DROP INDEX IF EXISTS current_state_overlay_objects_vault;
+DROP INDEX IF EXISTS current_state_overlay_objects_broker;
+DROP INDEX IF EXISTS current_state_overlay_objects_asset;
+DROP INDEX IF EXISTS current_state_overlay_objects_status;
+DROP INDEX IF EXISTS current_state_overlay_objects_ledger;
