ed558b79cf4abbc76ea3ae884bc50f06f518f876
badjoke-lab <badjoke.lab@gmail.com>
2026-07-24 23:32:31 +0900

    Index canonical transaction lookups

--- migrations/10000_fast_lane_transaction_lookup.sql
+++ migrations/10000_fast_lane_transaction_lookup.sql
@@ -3,6 +3,3 @@ CREATE INDEX IF NOT EXISTS idx_protocol_events_network_event_hash
 
 CREATE INDEX IF NOT EXISTS idx_object_changes_network_transaction_hash
   ON object_changes (network, transaction_hash, node_index, field_name);
-
-ALTER TABLE fast_lane_history_windows
-  ADD COLUMN transaction_hashes_json TEXT NOT NULL DEFAULT '[]';
