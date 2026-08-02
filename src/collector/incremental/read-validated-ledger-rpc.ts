349bb88eaa8ebd659aac8f7a90f7a3c5d045531a
badjoke-lab <badjoke.lab@gmail.com>
2026-08-02 11:35:30 +0900

    Move validated ledger RPC transport out of parser surface

---
+++ src/collector/incremental/read-validated-ledger-rpc.ts
@@ -0,0 +1,29 @@
+import { XrplJsonRpcClient, type FetchLike } from '../network/xrpl-rpc'
+import {
+  parseValidatedLedgerResult,
+  type ValidatedLedgerRead,
+} from './validated-ledger-parser'
+
+export async function readValidatedLedger(options: {
+  endpoint: string
+  ledgerIndex: number
+  timeoutMs: number
+  fetcher?: FetchLike
+}): Promise<ValidatedLedgerRead> {
+  const client = new XrplJsonRpcClient({
+    endpoint: options.endpoint,
+    timeoutMs: options.timeoutMs,
+    fetcher: options.fetcher,
+  })
+  const result = await client.call<Record<string, unknown>>('ledger', {
+    ledger_index: options.ledgerIndex,
+    transactions: true,
+    expand: true,
+    owner_funds: false,
+  })
+  return parseValidatedLedgerResult({
+    endpoint: options.endpoint,
+    requestedLedgerIndex: options.ledgerIndex,
+    result,
+  })
+}
